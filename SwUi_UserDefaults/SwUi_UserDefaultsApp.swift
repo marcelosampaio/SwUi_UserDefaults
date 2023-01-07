3f7996286cbec7aec7209c954bf709c71cf455fe
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2023-01-07 19:20:31 -0300

    Initial Commit

---
+++ SwUi_UserDefaults/SwUi_UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_UserDefaultsApp.swift
+//  SwUi_UserDefaults
+//
+//  Created by Marcelo Sampaio on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
